57a5bfb55fa334eef4d5f38a509410f865061be3
somacoderv <emanuelperez48@aragon.unam.mx>
2021-12-10 18:11:47 -0600

    csv

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application {
     try {
       Parent root = FXMLLoader.load(getClass().getResource("/fes/aragon/ui/App.fxml"));
       Scene scene = new Scene(root);
-      // scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       primaryStage.setResizable(false);
       primaryStage.setScene(scene);
       primaryStage.show();
